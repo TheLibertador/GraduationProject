ca57a170dba34dfbb7d4dd2f7f33fa77b520f195
masterxolos <ozgur-2000-6@hotmail.com>
2023-01-03 06:02:16 +0300

    add grid system

---
+++ Assets/Scripts/GridSystem/BuildingSystem.cs
@@ -0,0 +1,101 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class BuildingSystem : MonoBehaviour
+{
+    public static BuildingSystem current;
+
+    public GridLayout gridLayout;
+
+    private Grid grid;
+
+    [SerializeField] private Tilemap MainTilemap;
+
+    [SerializeField] private Tile whiteTile;
+
+    public GameObject prefab1;
+    public GameObject prefab2;
+
+    private PlacableObject objectToPlace;
+
+    private void Awake()
+    {
+        current = this;
+        grid = gridLayout.gameObject.GetComponent<Grid>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            InitializeWithObject(prefab1);
+        }
+        else if (Input.GetKeyDown(KeyCode.N))
+        {
+            InitializeWithObject(prefab2);
+        }
+    }
+
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            return raycastHit.point;
+        }
+        else
+        {
+            return Vector3.zero;
+        }
+    }
+    public Vector3 SnapCoordinateToGrid(Vector3 position)
+    {
+        Vector3Int cellPos = gridLayout.WorldToCell(position);
+        position = grid.GetCellCenterWorld(cellPos);
+        return position;
+    }
+
+    public void InitializeWithObject(GameObject prefab)
+    {
+        Vector3 position = SnapCoordinateToGrid(Vector3.zero);
+
+        GameObject obj = Instantiate(prefab, position, Quaternion.identity);
+        objectToPlace = obj.GetComponent<PlacableObject>();
+        obj.AddComponent<ObjectDrag>();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ Assets/Scripts/GridSystem/ObjectDrag.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDrag : MonoBehaviour
+{
+    private Vector3 offset;
+
+    private void OnMouseDown()
+    {
+        offset = transform.position - BuildingSystem.GetMouseWorldPosition();
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 pos = BuildingSystem.GetMouseWorldPosition() + offset;
+        transform.position = BuildingSystem.current.SnapCoordinateToGrid(pos);
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
