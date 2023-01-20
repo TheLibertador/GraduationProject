5525cde95c9c2689a764dda2c0722fd4419fb871
masterxolos <ozgur-2000-6@hotmail.com>
2023-01-20 04:52:10 +0300

    add Building system

--- Assets/Scripts/GridSystem/BuildingSystem.cs
+++ Assets/Scripts/GridSystem/BuildingSystem.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using UnityEngine.UI;
 
 public class BuildingSystem : MonoBehaviour
 {
@@ -19,8 +21,19 @@ public class BuildingSystem : MonoBehaviour
     public GameObject prefab1;
     public GameObject prefab2;
 
-    private PlacableObject m_ObjectToPlace;
+    private PlacableObject _ObjectToPlace;
+    [SerializeField] private Text _errorText;
 
+    /*
+    public class GridObject
+    {
+        public override string ToString()
+        {
+            return x + ", " + z;
+        }
+    }
+*/
+    
     private void Awake()
     {
         Current = this;
@@ -29,6 +42,7 @@ public class BuildingSystem : MonoBehaviour
 
     private void Update()
     {
+        /*
         if (Input.GetKeyDown(KeyCode.B))
         {
             InitializeWithObject(prefab1);
@@ -37,6 +51,63 @@ public class BuildingSystem : MonoBehaviour
         {
             InitializeWithObject(prefab2);
         }
+
+        if (!_ObjectToPlace)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (CanBePlaced(_ObjectToPlace))
+            {
+                _ObjectToPlace.Place();
+                Vector3Int start = gridLayout.WorldToCell(_ObjectToPlace.GetStartPosition());
+                TakeArea(start, _ObjectToPlace.Size);
+            }
+            else
+            {
+                Destroy((_ObjectToPlace.gameObject));
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Destroy(_ObjectToPlace.gameObject);
+        }
+        */
+        
+    }
+    
+    public void CheckIfCanPlaced()
+    {
+        if (!_ObjectToPlace)
+        {
+            return;
+        }
+        
+        if (CanBePlaced(_ObjectToPlace))
+        {
+            _ObjectToPlace.Place();
+            Vector3Int start = gridLayout.WorldToCell(_ObjectToPlace.GetStartPosition());
+            TakeArea(start, _ObjectToPlace.Size);
+        }
+        else
+        {
+            Destroy(_ObjectToPlace.gameObject);
+            StartCoroutine(DisplayError());
+        }
+    }
+
+    IEnumerator DisplayError()
+    {
+        _errorText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2);
+        _errorText.gameObject.SetActive(false);
+    }
+
+    public void DiscardBuilding()
+    {
+        Destroy(_ObjectToPlace.gameObject);
     }
 
     public static Vector3 GetMouseWorldPosition()
@@ -58,15 +129,56 @@ public class BuildingSystem : MonoBehaviour
         return position;
     }
 
+    private static TileBase[] GetTilesBlock(BoundsInt area, Tilemap tilemap)
+    {
+        TileBase[] array = new TileBase[area.size.x * area.size.y * area.size.z];
+        int counter = 0;
+
+        foreach (var v in area.allPositionsWithin)
+        {
+            Vector3Int pos = new Vector3Int(v.x, v.y, 0);
+            array[counter] = tilemap.GetTile(pos);
+            counter++;
+        }
+
+        return array;
+    }
+
     public void InitializeWithObject(GameObject prefab)
     {
         Vector3 position = SnapCoordinateToGrid(Vector3.zero);
 
         GameObject obj = Instantiate(prefab, position, Quaternion.identity);
-        m_ObjectToPlace = obj.GetComponent<PlacableObject>();
+        _ObjectToPlace = obj.GetComponent<PlacableObject>();
         obj.AddComponent<ObjectDrag>();
     }
-    
+
+    private bool CanBePlaced(PlacableObject placeableObject)
+    {
+        BoundsInt area = new BoundsInt();
+        area.position = gridLayout.WorldToCell(_ObjectToPlace.GetStartPosition());
+//        area.size = new Vector3Int(area.size.x + 1, area.size.y + 1, area.size.z);
+        area.size = new Vector3Int(placeableObject.Size.x + 1, placeableObject.Size.y + 1, placeableObject.Size.z);
+
+        TileBase[] baseArray = GetTilesBlock(area, mainTilemap);
+        foreach (var b in baseArray)
+        {
+            if (b == whiteTile)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void TakeArea(Vector3Int start, Vector3Int size)
+    {
+        mainTilemap.BoxFill(start, whiteTile, start.x,start.y,start.x+size.x,start.y+size.y);
+        Debug.Log(start.x);
+        Debug.Log(start.x+size.x);
+        Debug.Log(start.y);
+        Debug.Log(start.y+size.y);
+    }
     
     
     
--- Assets/Scripts/GridSystem/PlacableObject.cs
+++ Assets/Scripts/GridSystem/PlacableObject.cs
@@ -1,15 +1,63 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = System.Object;
 
 public class PlacableObject : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool Placed { get; private set; }
+    public Vector3Int Size { get; private set; }
+
+    private Vector3[] Vertices;
+
+    private void GetColliderVertexPositionsLocal()
+    {
+        BoxCollider b = gameObject.GetComponent<BoxCollider>();
+        Vertices = new Vector3[4];
+        Vertices[0] = b.center + new Vector3(-b.size.x, -b.size.y, -b.size.z) * 0.5f;
+        Vertices[1] = b.center + new Vector3(b.size.x, -b.size.y, -b.size.z) * 0.5f;
+        Vertices[2] = b.center + new Vector3(b.size.x, -b.size.y, b.size.z) * 0.5f;
+        Vertices[3] = b.center + new Vector3(-b.size.x, -b.size.y, b.size.z) * 0.5f;
+    }
+
+    private void CalculateSizeIncells()
+    {
+        Vector3Int[] vertices = new Vector3Int[Vertices.Length];
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Vector3 worldPos = transform.TransformPoint(Vertices[i]);
+            vertices[i] = BuildingSystem.Current.gridLayout.WorldToCell(worldPos);
+        }
+
+        Size = new Vector3Int(
+            Math.Abs((vertices[0] - vertices[1]).x),
+            Math.Abs((vertices[0] - vertices[3]).y),
+            1);
+    }
+
+    public Vector3 GetStartPosition()
+    {
+        return transform.TransformPoint(Vertices[0]);
+    }
+    
     void Start()
     {
+        GetColliderVertexPositionsLocal();
+        CalculateSizeIncells();
         
     }
 
+    public virtual void Place()
+    {
+        ObjectDrag drag = gameObject.GetComponent<ObjectDrag>();
+        Destroy(drag);
+
+        Placed = true;
+        
+        //burda bina koyunca olacak şeyleri yazabiliriz
+    }
     // Update is called once per frame
     void Update()
     {
