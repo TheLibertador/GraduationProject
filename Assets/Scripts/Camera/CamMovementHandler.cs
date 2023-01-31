b400bbfa0ff4ddd8c06866769c4a4544215387d5
ErdemKaya <kayaerdem936@gmail.com>
2023-01-31 18:10:42 +0300

    add cameraMovement

---
+++ Assets/Scripts/Camera/CamMovementHandler.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovementHandler : MonoBehaviour
+{
+   [SerializeField] private Vector3 camOffset;
+   [SerializeField] private float camLerpSpeed = 0.125f;
+   
+   private Transform m_TargetTransform;
+   private Vector3 m_DesiredCamPosition;
+   
+   private void Awake()
+   {
+      m_TargetTransform = GameObject.FindWithTag("Player").transform;
+   }
+
+   private void LateUpdate()
+   {
+      MoveCamera();
+      LookAtPlayer();
+   }
+
+
+   private void MoveCamera()
+   {
+      m_DesiredCamPosition = m_TargetTransform.position + camOffset;
+      //transform.position = Vector3.Lerp(transform.position, m_DesiredCamPosition, camLerpSpeed);
+      transform.position = m_DesiredCamPosition;
+
+   }
+
+   private void LookAtPlayer()
+   {
+      transform.LookAt(m_TargetTransform);
+   }
+}
