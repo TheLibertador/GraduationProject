64480366b5edbdd6cd117b240d8cc89b73c909b9
ErdemKaya <kayaerdem936@gmail.com>
2023-03-20 15:41:27 +0300

    Update ResourceDropper.cs

--- Assets/Scripts/ResourceSystem/ResourceDropper.cs
+++ Assets/Scripts/ResourceSystem/ResourceDropper.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 
 public class ResourceDropper : MonoBehaviour
 {
-    [SerializeField] private GameObject gold;
-    [SerializeField] private GameObject iron;
-    [SerializeField] private GameObject stone;
-    [SerializeField] private GameObject wood;
-    
+    [SerializeField] private  GameObject gold;
+    [SerializeField] private  GameObject iron;
+    [SerializeField] private  GameObject stone;
+    [SerializeField] private  GameObject wood;
 
-    private void DropResource(string id, int value, Transform position)
+    private Dictionary<string, GameObject> m_Resources = new Dictionary<string, GameObject>();
+
+    private void Awake()
     {
-        foreach (var item in ResourceManager.Instance.GetResourceDictionary())
+        m_Resources.Add("gold", gold);
+        m_Resources.Add("iron", iron);
+        m_Resources.Add("stone", stone);
+        m_Resources.Add("wood", wood);
+    }
+
+    private void DropResource(string id, int value, Vector3 position)
+    {
+        foreach (var item in m_Resources)
         {
             if (item.Key == id)
             {
-                //Instantiate<>(id, position);
+                Instantiate(item.Value, position, Quaternion.identity);
             }
         }
     }
